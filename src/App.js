09660f98eaf9dedc776d112271d638b77a42bde9
ananishimoto <ana.nishimoto@gmail.com>
2022-05-02 16:35:30 +0200

    Set up CRA, create some components

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>HomePage</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
