9a7c994241be427ef068db049d939d5c73df42f9
ananishimoto <ana.nishimoto@gmail.com>
2022-05-02 20:02:56 +0200

    Starting the Nav Bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <NavBar />
         <h1>HomePage</h1>
       </header>
     </div>
---
+++ src/components/NavBar/index.js
@@ -0,0 +1,9 @@
+import "./style.css";
+
+export default function NavBar() {
+  return (
+    <div className="nav-bar-container">
+      <header>This is the NavBar</header>
+    </div>
+  );
+}
