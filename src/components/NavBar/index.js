be342942254af0a403fe413fc8c29303497ff4b4
rayssalves <rayssa.a.vieira@outlook.com>
2022-05-02 22:09:42 +0200

    nav bar structure

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -3,13 +3,32 @@ import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink></NavLink>
-        </li>
-      </ul>
-    </nav>
+    <div class="navbar-container">
+      <div class="navbar-items">
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/about">TheShop</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">Products</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">Blog</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">Shop</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">Contact</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
   );
 };
 
