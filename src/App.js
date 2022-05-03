77947be20a675c22539fe8817f459f89d3ab4fff
ananishimoto <ana.nishimoto@gmail.com>
2022-05-03 15:30:05 +0200

    feat/header_css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import { NavBar } from "./components"; //now gathers all components in the directory
 import HomePage from "./pages/HomePage";
 import ShopPage from "./pages/ShopPage";
 import DetailsPage from "./pages/DetailsPage";
@@ -8,9 +7,6 @@ import DetailsPage from "./pages/DetailsPage";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <NavBar />
-      </header>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/shop" element={<ShopPage />} />
