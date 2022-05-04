9f64fc901ae4eac50dc18578b0de5c87b221e5a6
rayssalves <rayssa.a.vieira@outlook.com>
2022-05-04 14:54:04 +0200

    finishing details skeleton

--- src/pages/DetailsPage/index.js
+++ src/pages/DetailsPage/index.js
@@ -3,42 +3,36 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "./style.css";
 
-const DetailsPage = () => {};
+const DetailsPage = () => {
+  const routeParams = useParams();
+  const [details, setDetails] = useState(null);
+
+  useEffect(() => {
+    async function fetchDetails() {
+      const response = await axios.get(
+        `http://localhost:4000/products/${routeParams.id}`
+      );
+      console.log(response);
+      setDetails(response.data);
+    }
+    fetchDetails();
+  }, []);
+
+  return details ? (
+    <div>
+      <h1>{details.title}</h1>
+      <div>
+        <img src={details.mainImage} alt="product" />
+        <div>
+          <h2>€{details.price}</h2>
+          <p>{details.description}</p>
+          <p>{details.rating}</p>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <p>...</p>
+  );
+};
 
 export { DetailsPage };
-
-// function HDetailsPage() {
-//   const routeParams = useParams();
-//   const [house, setHouse] = useState(null);
-
-//   useEffect(() => {
-//     async function fetchHouseDetails() {
-//       const response = await axios.get(
-//         `https://hp-assessment-api.herokuapp.com/hp/house/${routeParams.id}`
-//       );
-
-//       console.log(response);
-//       setHouse(response.data);
-//     }
-//     fetchHouseDetails();
-//   }, []);
-
-//   return house ? (
-//     <div>
-//       <h1 class="house-details">{house.name}</h1>
-//       <div class="house-details-container">
-//         <img class="house-img" src={house.imgUrl} alt="Harry Potter houses" />
-//         <div class="content-house">
-//           <h2>{house.name}</h2>
-//           <p>House Animal : {house.animal}</p>
-//           <p>House Color : {house.colors}</p>
-//           <p>House Founder : {house.founder}</p>
-//           <p>House Ghost : {house.ghost}</p>
-//         </div>
-//       </div>
-//     </div>
-//   ) : (
-//     <p>..</p>
-//   );
-// }
-// export default HouseDetailsPage;
