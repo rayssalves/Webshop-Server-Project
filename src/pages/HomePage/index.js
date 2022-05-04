63dec756667da8ba9bdcd4e0d29172ac1e29d36a
rayssalves <rayssa.a.vieira@outlook.com>
2022-05-04 15:04:48 +0200

    fix/was importing navbar and header 2 times

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,13 +1,9 @@
-import { NavBar } from "../../components";
-import Header from "../../components/Header";
 import { NavLink } from "react-router-dom";
 import "./style.css";
 
 export default function ShopPage() {
   return (
     <div>
-      <NavBar />
-      <Header />
       <div className="homepage-body">
         <NavLink className="products-page-button" to="/shop">
           Our products
